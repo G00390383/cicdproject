e5997f3e8a9ee986f27ad61da927d7353925eadc
Darragh walker <g00390383@atu.ie>
2022-12-09 14:45:31 +0000

    Configured and created the final version of our cicd project

---
+++ src/test/java/ie/atu/microservices/MicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
