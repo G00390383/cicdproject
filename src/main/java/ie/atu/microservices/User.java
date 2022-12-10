6bae1d6d32284af8c9eab1595110b59eb03a6cc1
G00398070 <G00398070@atu.ie>
2022-12-10 11:56:01 +0000

    Added getters and setters. Added fail and success case tests to name

--- src/main/java/ie/atu/microservices/User.java
+++ src/main/java/ie/atu/microservices/User.java
@@ -1,4 +1,52 @@
 package ie.atu.microservices;
 
 public class User {
+    private String name;
+    private Long ppsnNo;
+    private int creditScore;
+    private String emailId;
+
+    public User(String name, Long ppsnNo, int creditScore, String emailId) {
+        setName(name);
+        this.ppsnNo = ppsnNo;
+        this.creditScore = creditScore;
+        this.emailId = emailId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if(name.length() >= 3) {
+            this.name = name;
+        }
+        else{
+            throw new IllegalArgumentException("Name must be 3 characters or longer");
+        }
+    }
+
+    public Long getPpsnNo() {
+        return ppsnNo;
+    }
+
+    public void setPpsnNo(Long ppsnNo) {
+        this.ppsnNo = ppsnNo;
+    }
+
+    public int getCreditScore() {
+        return creditScore;
+    }
+
+    public void setCreditScore(int creditScore) {
+        this.creditScore = creditScore;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
 }
