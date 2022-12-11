b2f222deb5e64518fedf037d8bd90ddc07ad98b8
Darragh walker <g00390383@atu.ie>
2022-12-11 21:50:22 +0000

    Code to update the users PPSN

--- src/main/java/ie/atu/microservices/Repo/UserRepo.java
+++ src/main/java/ie/atu/microservices/Repo/UserRepo.java
@@ -2,10 +2,12 @@ package ie.atu.microservices.Repo;
 
 import ie.atu.microservices.Model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepo extends MongoRepository<User, Integer> {
-    @Override
-    Optional<User> findById(Integer integer);
+
+    Optional<User> findById(int id);
 }
